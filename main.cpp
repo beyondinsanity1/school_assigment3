bff7f760df1f05c8559e79242fbf7c90d96238a9
beyondinsanity <ernsonmez@gmail.com>
2022-11-16 22:49:14 -0500

    a

--- main.cpp
+++ main.cpp
@@ -1,4 +1,65 @@
 #include <iostream>
 #include <string>
+#include <time.h>
 
 using namespace std;
+
+class player{
+private:
+    int HP;
+    string playerName;
+    int weaponModifier;
+public:
+    player(int theHP);
+    int weaponAttack();
+    void setHP(int);
+    int getHP();
+    int heal();
+};
+
+
+player::player(int theHP){
+    HP = theHP;
+}
+
+ void player::setHP(int theHP) {
+     HP = theHP;
+ }
+
+ int player::getHP() {
+     return HP;
+ }
+
+ int player::weaponAttack() {
+     int upper = 10;
+     int lower = 5;
+     int weaponAttackValue = (rand()%(upper - lower + 1) + lower);
+     weaponAttackValue = weaponAttackValue * weaponModifier;
+     return weaponAttackValue;
+ }
+
+class enemy{
+private:
+    int enemyHP;
+public:
+    enemy(){};
+    void enemyAttack();
+};
+
+void enemy::enemyAttack() {
+    int upper = 20;
+    int lower = 10;
+    int enemyAttackValue = (rand()%(upper - lower + 1) + lower);
+}
+
+int main(){
+
+    srand(time(NULL));
+    player myplayer(200);
+
+    cout << myplayer.getHP() <<endl;
+    cout << myplayer.weaponAttack() <<endl;
+    cout << enemy <<endl;
+
+
+}
